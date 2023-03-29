829805ba5107b0860332a67c4bac47b556573628
Bernard Olaires <bolairesjr@gmail.com>
2023-03-28 22:13:08 -0700

    finished the task form, and started on manager dashboard table

--- server/routes/task.routes.js
+++ server/routes/task.routes.js
@@ -3,7 +3,7 @@ const TaskController = require('../controllers/task.controller');
 module.exports = (app) => {
     app.post('/api/newTask', TaskController.createTask);
     app.get('/api/allTask', TaskController.getAllTask);
-    app.get('/api/task/:id', TaskController.getTask);
+    app.get('/api/oneTask/:id', TaskController.getTask);
     app.put('/api/task/:id', TaskController.updateTask);
     app.delete('/api/task/:id', TaskController.deleteTask);
 };
