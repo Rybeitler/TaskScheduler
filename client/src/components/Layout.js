e225ed631df9f36b7cc774f09daf67699c8c314d
ChrisCarino1 <106113286+ChrisCarino1@users.noreply.github.com>
2023-03-30 12:42:19 -0700

    navbar styling

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 
 
+
 const Layout = () => {
     return (
         <main className='App'>
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
-import './nav.css'
+import Navbar from './nav.module.css'
 
 const Nav = (props) => {
 
@@ -28,11 +28,26 @@ const Nav = (props) => {
     }
 
     return(
-        <div className='nav-container'>
-            <h1>Hi {auth?.user?.role}, {auth?.user?.firstName}!</h1>
-            <button onClick={handleHome}>Home</button>
-            <button onClick={handleLogout}>Logout</button>
-        </div>
+        // <div className='nav-container'>
+        //     <h1>Hi {auth?.user?.role}, {auth?.user?.firstName}!</h1>
+        //     <button onClick={handleHome}>Home</button>
+        //     <button onClick={handleLogout}>Logout</button>
+        // </div>
+        <body>
+            <nav>
+                <label class={Navbar.logo}>TaskScheduler</label>
+                <div class={Navbar.links}>
+                    {/* Left side links */}
+                    <ul class={Navbar.ul}>
+                        <li class={Navbar.li}><button class={Navbar.button} onClick={handleHome}>Home</button></li>
+                    </ul>
+                    {/* Right side links */}
+                    <ul class={Navbar.ul}> 
+                        <li class={Navbar.li}><button class={Navbar.button} onClick={handleLogout}>Logout</button></li>
+                    </ul>
+                </div>
+            </nav>
+        </body>
     )
 }
 
